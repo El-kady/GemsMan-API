720302e82bbee38f0ba28379dcc6ecae2d8d8b5d
MoustafaElkady <moustafa.m.elkady@gmail.com>
2017-06-18 14:08:15 +0200

    Fix group by bug in api

--- app/controllers/api/gems_controller.rb
+++ app/controllers/api/gems_controller.rb
@@ -1,7 +1,7 @@
 class Api::GemsController < ApplicationController
 
   def query
-    render json: RubyGem.joins(:libraries).where(name: params[:gems].to_a,:libraries => { :os_name => params[:os] }).group('ruby_gems.name').map{
+    render json: RubyGem.joins(:libraries).where(name: params[:gems].to_a,:libraries => { :os_name => params[:os] }).group('ruby_gems.id').map{
         |gem| {name: gem.name, libraries: gem.libraries.map{ |l| l.name }}
     }
   end
